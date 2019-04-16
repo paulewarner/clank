94770b10ec734e4c74f7863fef5e2b4a6d256b7e
Paul Warner <paul.eliot.warner@gmail.com>
2019-04-15 23:03:33 -0400

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+extern crate specs;
+extern crate rand;
+#[macro_use]
+extern crate log;
+extern crate fern;
+extern crate chrono;
+
+use std::sync::Arc;
+
+use specs::prelude::*;
+
+mod map;
+
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
+
+struct Combatant {
+    hp: u32,
+    ai: Arc<Fn(Entity, &Combatant, (&Entities, &ReadStorage<Combatant>), &mut Read<LazyUpdate>) + Send + Sync>
+}
+
+impl std::fmt::Debug for Combatant {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "Combatant {{ hp: {} }}", self.hp)
+    }
+}
+
+impl Component for Combatant {
+    type Storage = VecStorage<Self>;
+}
+
+struct CombatSystem;
+
+impl<'a> System<'a> for CombatSystem {
+
+    type SystemData = (Entities<'a>, ReadStorage<'a, Combatant>, Read<'a, LazyUpdate>);
+
+    fn run(&mut self, (entities, combatants, mut updater): Self::SystemData) {
+
+        for (entity, combatant) in (&entities, &combatants).join() {
+            let f = combatant.ai.clone();
+            f(entity, combatant, (&entities, &combatants), &mut updater);
+        }
+    }
+}
+
+fn main() {
+    setup_logger().expect("Failed to setup logging");
+    let mut world = World::new();
+    world.register::<Combatant>();
+    world.register::<map::Presence>();
+    world.register::<map::Move>();
+    world.register::<map::Space>();
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(CombatSystem, "combat", &[])
+        .with(map::MapSystem, "map", &[]).build();
+
+    let person = world.create_entity()
+        .with(map::Presence)
+        .build();
+
+
+    let empty_space = world.create_entity()
+        .with(map::Space::new(true))
+        .build();
+
+    world.create_entity()
+        .with(map::Space::new_with_contents(true, person))
+        .with(map::Move::to(empty_space))
+        .build();
+
+    dispatcher.setup(&mut world.res);
+    dispatcher.dispatch(&mut world.res);
+}
---
+++ src/map.rs
@@ -0,0 +1,89 @@
+use specs::prelude::*;
+
+pub struct Presence;
+
+impl Component for Presence {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct Space {
+    passable: bool,
+    contents: Option<Entity>
+}
+
+impl Component for Space {
+    type Storage = VecStorage<Self>;
+}
+
+impl Space {
+    pub fn new(passable: bool) -> Space {
+        Space {
+            passable,
+            contents: None
+        }
+    }
+
+    pub fn new_with_contents(passable: bool, contents: Entity) -> Space {
+        Space {
+            passable,
+            contents: Some(contents)
+        }
+    }
+}
+
+pub struct Move {
+    to: Entity
+}
+
+impl Move {
+
+    pub fn to(e: Entity) -> Move {
+        Move {
+            to: e
+        }
+    }
+}
+
+impl Component for Move {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct MapSystem;
+
+impl<'a> System<'a> for MapSystem {
+    type SystemData = (Entities<'a>, WriteStorage<'a, Space>, WriteStorage<'a, Move>);
+
+    fn run(&mut self, (entities, mut spaces, mut moves): Self::SystemData) {
+        let mut to_remove = vec![];
+
+        for (entity, mv) in (&entities, &moves).join() {
+            trace!("Moving character...");
+            let mut contents: Option<Entity> = None;
+
+            if let Some(current) = spaces.get_mut(entity) {
+                contents = current.contents;
+                current.contents = None;
+            } else {
+                warn!("Failing to remove contents from space due to missing contents")
+            }
+
+            if let Some(target) = spaces.get_mut(mv.to) {
+                if target.passable == true && target.contents.is_none() {
+                    target.contents = contents;
+                    trace!("Character moved!");
+                } else {
+                    warn!("Cannot move to target space!");
+                }
+            } else {
+                warn!("Failed to find target space!");
+            }
+
+            to_remove.push(entity);
+        }
+
+        for mv in to_remove {
+            moves.remove(mv);
+        }
+    }
+}
+
